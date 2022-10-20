4a1668d16de51f7c3cae6ea329ae1a20f0cd6333
Kamil Kasiak <kasiakkamil@gmail.com>
2022-10-20 18:29:41 +0200

    category routing

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -12,10 +12,14 @@ const Categories = () => {
   return (
     <div className='bg-white shadow-lg rounded-lg p-8 mb-8 pb-12'>
       <h3 className='text-xl mb-8 font-semibold border-b pb-4'>Continents</h3>
+      <Link href='/'>
+        <span className='cursor-pointer block pb-2 mb-3'>Wszystkie posty</span>
+      </Link>
       {categories.map((category) => (
         <Link href={`/category/${category.slug}`} key={category.slug}>
           <span className='cursor-pointer block pb-2 mb-3'>
             {category.name}
+            {console.log(category)}
           </span>
         </Link>
       ))}
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import 'tailwindcss/tailwind.css';
 import { PostCard, Categories, PostWidget } from '../components';
 import { FeaturedPosts } from '../sections';
 import { getPosts } from '../services';
-import Loader from 'react-loaders';
 
 export default function Home({ posts }) {
   return (
@@ -18,7 +17,7 @@ export default function Home({ posts }) {
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
           {posts.map((post, index) => (
-            <PostCard post={post.node} key={post.title} />
+            <PostCard post={post.node} key={index} />
           ))}
         </div>
         <div className='lg:col-span-4 col-span-1'>
